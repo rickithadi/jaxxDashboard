ba4e55b9e4ce2744119a8a46cc7d95961cd966de
Hadi Rickit <rickithadi@gmail.com>
2022-09-11 16:10:11 +0800

    create user type, add context to track auth state, scaffold dashboard/login routing

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import "./App.css";
+import { Dashboard } from "./components/Dashboard";
+import { authContext } from "./context";
+
+function App() {
+  const { user } = useContext(authContext);
+  return (
+    <div className="App">
+      {user ? (
+        <Dashboard />
+      ) : (
+        <div>
+          <h1>log in</h1>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { authContext } from "./context";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+// create user context
+
+root.render(
+  <React.StrictMode>
+    <authContext.Provider value={"bob"}>
+      <App />
+    </authContext.Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
